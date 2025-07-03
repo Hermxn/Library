45b85a698d1d43d8d4a0b98eccc8d5ed49005f44
Hermxn <hermanhrynko@gmail.com>
2025-07-03 15:59:06 +0200

    Login and Signup pages unified in Auth page, App routing fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import { Home, Auth } from "./pages/_index";
 
 const App = () => {
   return (
@@ -12,8 +10,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login/" element={<Login />} />
-          <Route path="/signup/" element={<Signup />} />
+          <Route path="/login/" element={<Auth formtype="login" />} />
+          <Route path="/signup/" element={<Auth formtype="signup" />} />
         </Routes>
       </AuthProvider>
     </>
