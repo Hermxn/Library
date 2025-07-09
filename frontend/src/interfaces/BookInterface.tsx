a5d5a9d4c7a2756763e4483f079eeabc042302b2
Hermxn <hermanhrynko@gmail.com>
2025-07-09 14:04:54 +0200

    Component Book created

---
+++ frontend/src/interfaces/BookInterface.tsx
@@ -0,0 +1,14 @@
+export default interface BookInterface {
+  id: string;
+  bookName: string;
+  author: string;
+  annotation: string;
+  description: string;
+  publisher: string;
+  genre: string;
+  pages: string;
+  year: string;
+  availableStock: string;
+  fullStock: string;
+  image: string;
+}
